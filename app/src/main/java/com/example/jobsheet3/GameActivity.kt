c1877f1cf63a9eb5fb3cd53efd973fe8e4aab20e
RafiKurnia01 <rafikurniailahi00@gmail.com>
2023-12-06 15:19:40 +0700

    Build Janken games with Kotlin in Android Studio

---
+++ app/src/main/java/com/example/jobsheet3/GameActivity.kt
@@ -0,0 +1,47 @@
+package com.example.jobsheet3
+
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class GameActivity : AppCompatActivity() {
+    private lateinit var computerImageView: ImageView
+    private lateinit var rockImageButton: ImageButton
+    private lateinit var scissorsImageButton: ImageButton
+    private lateinit var paperImageButton: ImageButton
+    private lateinit var resultImageView: ImageView
+
+    private fun initComponents(){
+        computerImageView = findViewById(R.id.computerImageview)
+        rockImageButton = findViewById(R.id.rockImagebutton)
+        scissorsImageButton = findViewById(R.id.scissorsImagebutton)
+        paperImageButton = findViewById(R.id.paperImagebutton)
+        resultImageView = findViewById(R.id.resultImageview)
+    }
+
+    private fun initListeners(){
+        rockImageButton.setOnClickListener{startGame("ROCK")}
+        scissorsImageButton.setOnClickListener{startGame("SCISSORS")}
+        paperImageButton.setOnClickListener { startGame("PAPER") }
+    }
+
+    private fun startGame(option: String){
+        val computerOption = Game.pickRandomOption()
+        computerImageView.setImageResource(Game.pickDrawable(computerOption))
+
+        when{
+            Game.isDraw(option, computerOption) -> resultImageView.setImageResource(R.drawable.draw)
+            Game.isWin(option, computerOption) -> resultImageView.setImageResource(R.drawable.win)
+            else -> resultImageView.setImageResource(R.drawable.lose)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.game)
+
+        initComponents()
+        initListeners()
+    }
+}
